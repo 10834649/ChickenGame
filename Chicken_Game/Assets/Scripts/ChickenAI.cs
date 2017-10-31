733349e9af9dda3f2086217f5febb8a81d912c5e
Unknown <10834649@my.uvu.edu>
2017-10-30 20:11:14 -0600

    Day Three
    vanilla chickenAI and vanilla points system

---
+++ Chicken_Game/Assets/Scripts/ChickenAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAI : MonoBehaviour {
+
+	public Rigidbody enemy;
+	public float moveSpeed;
+	public Transform target;
+	public Transform chickenPen;
+
+	public int points = 10;
+	// chicken runaway trigger
+	void OnTriggerStay(Collider other){
+
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Player has entered chickens trigger");
+
+			transform.LookAt(target);
+
+			transform.Translate(Vector0.back*moveSpeed*Time.deltatime);
+		}
+	}
+
+	// Chicken random movement
+	// Void
+
+	// Chicken transport to pen void	
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.name == "Player"){
+			// Add points to score
+			ScorManager.AddPoints(points);
+			// send chicken to chicken pen
+			transform.position = chickenPen.position;
+			transform.rotation = chickenPen.position;
+
+		}
+	}
+}
